095e4d898765db9dfa646ad9503bcd039bc8e26a
cloudy64 <zainobo1690@gmail.com>
2025-10-12 14:05:47 +0300

    done

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,65 +3,85 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
-
-router.get('/',async(req,res)=>{
-
-  try {
+router.get('/', async (req, res) => {
+    try {
        const currentUser = await User.findById(req.session.user._id);
        res.render('foods/index.ejs', {
         pantry: currentUser.pantry,
-        HasEmptyPantry: currentUser.pantry.length === 0, 
-       });
+        HasEmptyPantry: currentUser.pantry.length === 0,
+       }); 
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+    
+});
+//in server.js i use app.get here we cannot use i want to use little bit from the server 
+
+router.get('/new', (req, res) => {
+    res.render('foods/new.ejs');
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id); // will get us the full user object, including the foods schema
+        currentUser.pantry.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
     } catch (error) {
         console.log(error);
         res.redirect('/');
     }
 });
-//in server.js i use app.get here we cannot use i want to use little bit from the server 
 
-router.get('/new',(req,res)=>{
-  res.render('foods/new.ejs');
-})
-
-router.post('/',async(req,res)=>{
-  try{
-    const currentUser = await User.findById(req.session.user._id);
-    currentUser.pantry.push(req.body); //dealing with array so .push eveything 
-    await currentUser.save();
-    res.redirect(`/users/${currentUser._id}/foods`); //redirect goes to server.js it wont put in mind the /user/id the one dont change
-  }catch(error){
-    console.log(error);
-    res.redirect('/');
-  };
-})
-
-router.get('/:foodId', async (req, res) => {
-  try {
-    const currentUser = await User.findById(req.session.user._id);
-
-    const food = currentUser.pantry.id(req.params.foodId);
-
-    res.render('foods/show.ejs', {
-      food: food,
-    });
-  } catch (error) {
-
-    console.log(error);
-    res.redirect('/');
-  }
+router.get('/:itemId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const pantryItem = currentUser.pantry.id(req.params.itemId);
+        res.render('foods/show.ejs', {
+        pantryItem,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
 });
 
-router.delete('/:foodId', async (req, res) => {
+router.delete('/:itemId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.id(req.params.itemId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.get('/:itemId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const pantryItem = currentUser.pantry.id(req.params.itemId);
+        res.render('foods/edit.ejs', {
+            pantryItem,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.put('/:itemId', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
-    
-    currentUser.pantry.id(req.params.foodId).deleteOne();
-
+    const pantryItem = currentUser.pantry.id(req.params.itemId);
+    pantryItem.set(req.body);
     await currentUser.save();
- 
-    res.redirect(`/users/${currentUser._id}/foods`);
+    res.redirect(
+      `/users/${currentUser._id}/foods/${req.params.itemId}`
+    );
   } catch (error) {
-
     console.log(error);
     res.redirect('/');
   }
--- controllers/users.js
+++ controllers/users.js
@@ -29,4 +29,5 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
+
 module.exports = router;
